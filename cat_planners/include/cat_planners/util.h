c647e8f675bead8e9aa75298c93b108780748695
Adam Leeper <aleeper@willowgarage.com>
2012-10-01 15:59:24 -0700

    port and rename 'local_planners' from jks-ros-pkg

---
+++ cat_planners/include/cat_planners/util.h
@@ -0,0 +1,119 @@
+#ifndef LOCAL_PLANNERS_UTIL_H
+#define LOCAL_PLANNERS_UTIL_H
+
+#include <ros/ros.h>
+
+#include <urdf_model/model.h>
+#include <moveit_msgs/GetMotionPlan.h>
+#include <moveit_msgs/MotionPlanDetailedResponse.h>
+#include <planning_scene/planning_scene.h>
+#include <planning_interface/planning_interface.h>
+#include <planning_models/conversions.h>
+
+
+namespace local_planners {
+
+  inline double normalizeAngle( double angle )
+  {
+    if(angle < -M_PI || angle > M_PI)
+    {
+      return angle - copysign(1.0, angle)*2*M_PI*ceil(fabs(angle)/ (2*M_PI));
+    }
+    return angle;
+  }
+
+
+
+  inline void printCollisionInfo(const planning_scene::PlanningScene& ps, const planning_models::KinematicState& ks )
+  {
+
+    collision_detection::CollisionRequest req;
+    req.max_contacts = 50;
+    req.contacts = true;
+    req.distance = false;
+    req.verbose = false;
+    collision_detection::CollisionResult res;
+    ps.checkCollision(req, res, ks);
+
+    ROS_INFO("Contact count: %zd", res.contact_count);
+    if(res.collision)
+      for( collision_detection::CollisionResult::ContactMap::iterator it = res.contacts.begin(); it != res.contacts.end(); ++it)
+      {
+        std::string contact1 = it->first.first;
+        std::string contact2 = it->first.second;
+        std::vector<collision_detection::Contact>& vec = it->second;
+
+        for(size_t contact_index = 0; contact_index < vec.size(); contact_index++)
+        {
+          Eigen::Vector3d pos =     vec[contact_index].pos;
+          Eigen::Vector3d normal =  vec[contact_index].normal;
+          double depth = vec[contact_index].depth;
+          ROS_INFO("Contact between [%s] and [%s] point: %.2f %.2f %.2f normal: %.2f %.2f %.2f depth: %.3f",
+                   contact1.c_str(), contact2.c_str(),
+                   pos(0), pos(1), pos(2),
+                   normal(0), normal(1), normal(2),
+                   depth);
+        }
+
+      }
+  }
+
+  inline void createKinematicStatePoint(const planning_scene::PlanningSceneConstPtr& planning_scene,
+                                        planning_models::KinematicState &start_state,
+                                        planning_models::KinematicState &goal_state,
+                                        std::map<std::string, double> &name_map,
+                                        double step_size,
+                                        planning_models::KinematicStatePtr &point)
+  {
+    for (std::map<std::string, double>::const_iterator it = name_map.begin() ; it != name_map.end() ; ++it)
+    {
+      double sv = start_state.getJointState(it->first)->getVariableValues()[0];
+      double gv = goal_state.getJointState(it->first)->getVariableValues()[0];
+      std::vector<moveit_msgs::JointLimits> limits = planning_scene->getKinematicModel()->getJointModel(it->first)->getLimits();
+      double u = 0;
+      //moveit_msgs::JointLimits &limit = limits[0];
+
+      gv = normalizeAngle(gv);
+      sv = normalizeAngle(sv);
+      // Handle wrap around
+      double delta = gv - sv;
+      step_size = std::min(step_size, fabs(delta));
+      if( delta >= M_PI || delta <= -M_PI )
+        u = sv - copysign(step_size, delta);
+      else
+        u = sv + copysign(step_size, delta);
+
+      point->getJointState(it->first)->setVariableValues(&u);
+    }
+  }
+
+  inline void createKinematicStatePoint(const planning_scene::PlanningSceneConstPtr& planning_scene,
+                                 planning_models::KinematicState &start_state, planning_models::KinematicState &goal_state,
+                                 std::map<std::string, double> &name_map,
+                                 unsigned int step_index, unsigned int num_steps,
+                                 planning_models::KinematicStatePtr &point)
+  {
+    for (std::map<std::string, double>::const_iterator it = name_map.begin() ; it != name_map.end() ; ++it)
+    {
+      double sv = start_state.getJointState(it->first)->getVariableValues()[0];
+      double gv = goal_state.getJointState(it->first)->getVariableValues()[0];
+      std::vector<moveit_msgs::JointLimits> limits = planning_scene->getKinematicModel()->getJointModel(it->first)->getLimits();
+      double u = 0;
+      //moveit_msgs::JointLimits &limit = limits[0];
+
+      gv = normalizeAngle(gv);
+      sv = normalizeAngle(sv);
+      // Handle wrap around
+      double delta = gv - sv;
+      if( delta >= M_PI || delta <= -M_PI )
+        u = sv - step_index * delta/(double)num_steps;
+      else
+        u = sv + step_index * delta/(double)num_steps;
+
+      point->getJointState(it->first)->setVariableValues(&u);
+    }
+  }
+
+}
+
+#endif
---
+++ cat_planners/src/linear_joint_stepper.cpp
@@ -0,0 +1,108 @@
+#include <cat_planners/util.h>
+#include <cat_planners/linear_joint_stepper.h>
+#include <pluginlib/class_list_macros.h>
+#include <planning_models/kinematic_model.h>
+#include <planning_interface/planning_interface.h>
+
+
+namespace cat_planners{
+
+bool LinearJointStepper::solve(const planning_scene::PlanningSceneConstPtr& planning_scene,
+                   const moveit_msgs::GetMotionPlan::Request &req,
+                   moveit_msgs::GetMotionPlan::Response &res) const
+{
+  ROS_WARN("Solving using LinearJointStepper!");
+
+  planning_models::KinematicState start_state = planning_scene->getCurrentState();
+  planning_models::robotStateToKinematicState(*(planning_scene->getTransforms()), req.motion_plan_request.start_state, start_state);
+
+  planning_models::KinematicState goal_state = start_state;
+  const moveit_msgs::Constraints &c = req.motion_plan_request.goal_constraints[0];
+  std::map<std::string, double> update;
+  for (std::size_t i = 0 ; i < c.joint_constraints.size() ; ++i)
+  {
+    update[c.joint_constraints[i].joint_name] = c.joint_constraints[i].position;
+  }
+  goal_state.setStateValues(update);
+  unsigned int steps = 1  + (start_state.distance(goal_state) / 0.03);
+//  steps = std::min<unsigned int>(steps, 6);
+  unsigned int MAX_STEPS = 6;
+
+  std::vector<planning_models::KinematicStatePtr> path;
+  if(planning_scene->isStateValid(start_state))
+    path.push_back(planning_models::KinematicStatePtr(new planning_models::KinematicState(start_state)));
+  else
+  {
+    ROS_ERROR("Can't plan, start state is not valid.");
+
+    //printCollisionInfo(*planning_scene, start_state);
+
+    return false;
+  }
+
+
+  if (steps < 3)
+  {
+    //planning_models::KinematicStatePtr point = planning_models::KinematicStatePtr(new planning_models::KinematicState(goal_state));
+
+    if (planning_scene->isStateValid(goal_state))
+      path.push_back(planning_models::KinematicStatePtr(new planning_models::KinematicState(goal_state)));
+  }
+  else
+  {
+    for (size_t s = 1 ; s <= MAX_STEPS ; ++s)
+    {
+      planning_models::KinematicStatePtr point = planning_models::KinematicStatePtr(new planning_models::KinematicState(start_state));
+      createKinematicStatePoint(planning_scene, start_state, goal_state, update, s, steps, point);
+      if (planning_scene->isStateValid(*point))
+        path.push_back(point);
+      else
+      {
+        //printCollisionInfo(*planning_scene, *point);
+        break;
+      }
+    }
+  }
+
+  // convert path to result trajectory
+  moveit_msgs::RobotTrajectory rt;
+  trajectory_msgs::JointTrajectory traj;
+
+  // Just take the last point
+  //for( size_t path_index = path.size()-1; path_index < path.size(); path_index++)
+  {
+
+    sensor_msgs::JointState js;
+    planning_models::kinematicStateToJointState(*(path.back()), js);
+    trajectory_msgs::JointTrajectoryPoint pt;
+
+    const planning_models::KinematicModel::JointModelGroup *jmg = planning_scene->getKinematicModel()->getJointModelGroup(req.motion_plan_request.group_name);
+
+    // getJointNames
+    for(size_t i = 0 ; i < js.name.size(); i++)
+    {
+      std::string name = js.name[i];
+      if( !jmg->hasJointModel(name) ) continue;
+
+      //if( path_index == 0)
+        traj.joint_names.push_back(js.name[i]);
+      pt.positions.push_back(js.position[i]);
+      if(js.velocity.size()) pt.velocities.push_back(js.velocity[i]);
+    }
+    pt.time_from_start = ros::Duration(0.1);
+    traj.points.push_back(pt);
+  }
+  traj.header.stamp = ros::Time::now();
+  traj.header.frame_id = "odom_combined";
+
+  res.trajectory.joint_trajectory = traj;
+
+  return true;
+}
+
+} // namespace cat_planners
+
+PLUGINLIB_DECLARE_CLASS(cat_planners, LinearJointStepper,
+                        cat_planners::LinearJointStepper,
+                        planning_interface::Planner)
+
